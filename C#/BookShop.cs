b6973f68d0d77a6b7097d37e47c99ef6f40d668d
[200~shruti-03-9-03 <ishrutiingle@gmail.com>
2024-08-25 22:35:50 +0530

    Bookshop using in oop

---
+++ C#/BookShop.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop
+{
+    public class Book
+    {
+        
+        private string author;
+        private string title;
+        private decimal price;
+        private string publisher;
+        public void AssignValues(string author,string title,decimal price,string publisher)
+        {
+            this.author = author;
+            this.title = title;
+            this.price = price;
+            this.publisher = publisher;
+        }
+        public void DisplayValues()
+        {
+            Console.WriteLine("Author:" +author);
+            Console.WriteLine("Title:" +title);
+            Console.WriteLine("Price: "+price); 
+            Console.WriteLine("Publisher: "+publisher);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Book myBook = new Book();
+
+           
+            myBook.AssignValues("walter isaacson","steve jobs",500,"simon & schuster");
+            myBook.DisplayValues();
+
+           
+            Console.Read();
+        }
+    }
+}
