a98ffad78f1ffaedec002ed06016af0ffd9d9780
[200~shruti-03-9-03 <ishrutiingle@gmail.com>
2024-08-26 17:40:24 +0530

    to check the number id even or odd

---
+++ C/EvenOROdd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+        printf("Enter an integer: ");
+    scanf("%d", &number);
+
+        if (number % 2 == 0) {
+        printf("%d is an even integer\n", number);
+    } else {
+        printf("%d is an odd integer\n", number);
+    }
+
+    return 0;
+}
