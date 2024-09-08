3045a511854e676837b250b873c650778c54ffe0
[200~shruti-03-9-03 <ishrutiingle@gmail.com>
2024-09-08 18:14:20 +0530

    the factorial of the number

--- C/Factorial.c
+++ C/Factorial.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int number,Cout;
+    int num, Cout;
     unsigned long long factorial = 1;
 
-    printf("Input the number: ");
-    scanf("%d", &number);
-
     
-    if (number < 0) {
+    printf("Input the number : ");
+    scanf("%d", &num);
+
+   
+    if (num < 0)
         printf("Factorial of a negative number doesn't exist.\n");
-    } else{
-       
-        for (Cout = 1; Cout<= number; ++Cout) {
+    else {
+        for(Cout = 1; Cout <= num; ++Cout) {
             factorial *= Cout;
         }
-        printf("The Factorial of %d is: %llu\n", number, factorial);
+        printf("The Factorial of %d is: %llu\n", num, factorial);
     }
 
-    return 0;
-}
+    return 0;}
