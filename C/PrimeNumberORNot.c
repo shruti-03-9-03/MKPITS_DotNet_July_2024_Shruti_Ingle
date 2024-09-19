016a36c4bf5240d1b05fcf7b6decf5ceda1f4efd
Shruti Ingle <ishrutiingle@gmail.com>
2024-09-19 12:28:59 +0530

    the given number is prime or not

---
+++ C/PrimeNumberORNot.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function to check if a number is prime
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false; // Numbers less than or equal to 1 are not prime
+    }
+    if (num <= 3) {
+        return true; // 2 and 3 are prime numbers
+    }
+    if (num % 2 == 0 || num % 3 == 0) {
+        return false; // Numbers divisible by 2 or 3 are not prime
+    }
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) {
+            return false; // Check divisibility by numbers of the form 6k ± 1
+        }
+    }
+    return true;
+}
+
+int main() {
+    int number;
+
+    // Input a number
+    printf("Input a number: ");
+    scanf("%d", &number);
+
+    // Check if the number is prime
+    if (isPrime(number)) {
+        printf("%d is a prime number.\n", number);
+    } else {
+        printf("%d is not a prime number.\n", number);
+    }
+
+    return 0;
+}
