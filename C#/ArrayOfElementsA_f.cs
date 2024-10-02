1fc1bab3bdb7642531866021aeb164cc2844ee4f
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-02 12:24:41 +0530

    Array Examples

---
+++ C#/ArrayOfElementsA_f.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Character array with elements from A to F
+        char[] charArray = { 'A', 'B', 'C', 'D', 'E', 'F' };
+
+        // Print the elements of the character array individually
+        Console.WriteLine(charArray[0]);
+        Console.WriteLine(charArray[1]);
+        Console.WriteLine(charArray[2]);
+        Console.WriteLine(charArray[3]);
+        Console.WriteLine(charArray[4]);
+        Console.WriteLine(charArray[5]);
+    }
+}
---
+++ C#/ElementsWithoutUsingArray.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Initial element of the array is 2000
+        int[] array = { 2000, 2001, 2002, 2003, 2004, 2005 };
+
+        // Print the elements individually without using a loop
+        Console.WriteLine(array[0]);
+        Console.WriteLine(array[1]);
+        Console.WriteLine(array[2]);
+        Console.WriteLine(array[3]);
+        Console.WriteLine(array[4]);
+        Console.WriteLine(array[5]);
+    }
+}
