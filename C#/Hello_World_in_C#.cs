1d63574cd55d28fd4a02bac7e78640fa82e52d11
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-03 11:15:44 +0530

    The Hello World In C#

---
+++ C#/Hello_World_in_C#.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Print "Hello World" without using WriteLine
+        Console.Write("Hello World");
+        Console.Write(Environment.NewLine); // To add a newline
+    }
+}
