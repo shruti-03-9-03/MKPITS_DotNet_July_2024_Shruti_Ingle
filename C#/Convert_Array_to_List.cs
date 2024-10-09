aef85f4dfcc52db87a6f18855566e2d1a5b94016
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-09 17:03:37 +0530

    Convert_Array_To_List

---
+++ C#/Convert_Array_to_List.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Array_to_List
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numberArray = { 1, 2, 3, 4, 5 };
+
+            List<int> numberList = ConvertArrayToList(numberArray);
+
+            Console.WriteLine("List elements:");
+            foreach (int number in numberList)
+            {
+                Console.WriteLine(number);
+            }
+            Console.Read();
+        }
+
+        static List<int> ConvertArrayToList(int[] array)
+        {
+            return new List<int>(array);
+        }
+    }
+}
