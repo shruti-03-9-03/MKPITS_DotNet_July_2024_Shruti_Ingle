99e5c764e0ab58e4df91ac39e0bbd33faeda5d14
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-09 17:22:23 +0530

    Convert list to a comma separated string

---
+++ C#/Convert_List_to_a_Comma_Separated_String.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_List_to_a_Comma_Separated_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string> { "apple", "banana", "cherry", "date" };
+
+            string commaSeparated = ConvertListToCommaSeparatedString(words);
+
+            Console.WriteLine("Comma-separated string:");
+            Console.WriteLine(commaSeparated);
+            Console.Read();
+        }
+
+        static string ConvertListToCommaSeparatedString(List<string> list)
+        {
+            return string.Join(", ", list);
+        }
+    }
+}
