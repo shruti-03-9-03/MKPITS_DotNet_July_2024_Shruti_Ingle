cd446f83751a52d00344e76b8c933b77e899faa5
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-09 17:18:05 +0530

    Remove duplicate elements from a list

---
+++ C#/Remove_Duplicate_Elements_from_a_List.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Duplicate_Elements_from_a_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5, 5, 5, 6 };
+
+            List<int> uniqueNumbers = RemoveDuplicates(numbers);
+
+            Console.WriteLine("List after removing duplicates:");
+            foreach (int number in uniqueNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.Read();
+        }
+
+        static List<int> RemoveDuplicates(List<int> list)
+        {
+            HashSet<int> uniqueSet = new HashSet<int>(list);
+            return new List<int>(uniqueSet);
+        }
+    }
+}
+
