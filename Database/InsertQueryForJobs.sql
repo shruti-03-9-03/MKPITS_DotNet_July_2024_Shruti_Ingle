5a925bf0777684f91e4ddfa6dae402f1f53ddd64
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-09 13:13:36 +0530

    The insert query for jobs

---
+++ Database/InsertQueryForJobs.sql
@@ -0,0 +1,27 @@
+Insert into jobs(jobtitle,MinimumSalary,MaximumSalary)
+values('Human Resource',20000.00,50000.00);
+Insert into jobs(jobtitle,MinimumSalary,MaximumSalary)
+values
+('Human Resource',20000.00,50000.00),
+('HR',20000.00,50000.00),
+('Human relation',20000.00,50000.00),
+('Human Resource',20000.00,50000.00),
+('Software developer',20000.00,50000.00),
+('Human Resource',20000.00,50000.00),
+('Human Resource',20000.00,50000.00),
+('Engineer',20000.00,50000.00),
+('mech engineer',20000.00,50000.00),
+('computer engineer',20000.00,50000.00),
+('computer tester',20000.00,50000.00),
+('enginerr',20000.00,50000.00),
+('software testing',20000.00,50000.00),
+('computer engineer',20000.00,50000.00),
+('Human Resource',20000.00,50000.00),
+('Human Resource',20000.00,50000.00),
+('computer engineer',20000.00,50000.00),
+('Human Resource',20000.00,50000.00),
+('computer engineer',20000.00,50000.00),
+('electronic engineer',20000.00,50000.00),
+('Human Resource',20000.00,50000.00),
+('computer enginer',20000.00,50000.00),
+('Human Resource',20000.00,50000.00);
