4cd13e219f5557cf3a8b1e431d33b8adba2e7041
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-09 16:41:29 +0530

    Add Elements To a List Integration

---
+++ C#/Add_Elements_To_A_List_Integers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Elements_To_List_Of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            AddElementsToList(numbers, new List<int> { 1, 2, 3, 4, 5 });
+        }
+
+        static void AddElementsToList(List<int> list, List<int> elementsToAdd)
+        {
+            list.AddRange(elementsToAdd);
+
+            Console.WriteLine("List elements:");
+            foreach (int number in list)
+            {
+                Console.WriteLine(number);
+            }
+            Console.Read();
+
+        }
+    }
+}
