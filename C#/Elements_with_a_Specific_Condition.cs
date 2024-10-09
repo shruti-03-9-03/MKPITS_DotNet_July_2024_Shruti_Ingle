987bfefb71a30e11acaca9e2d6ed55480632abc6
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-09 17:07:52 +0530

    Elements with a specific condition

---
+++ C#/Elements_with_a_Specific_Condition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Elements_with_a_Specific_Condition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            List<int> evenNumbers = GetEvenNumbers(numbers);
+
+            Console.WriteLine("Even numbers:");
+            foreach (int number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.Read();
+        }
+
+        static List<int> GetEvenNumbers(List<int> list)
+        {
+            return list.Where(number => number % 2 == 0).ToList();
+        }
+    }
+}
