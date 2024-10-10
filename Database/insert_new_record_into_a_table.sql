8bb5022b6e1fa6666ed731de467e1681d472d483
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-10 21:23:08 +0530

    The new insert record into a table

---
+++ Database/insert_new_record_into_a_table.sql
@@ -0,0 +1,11 @@
+--write a query to insert new record into a table
+insert into EmployeeDetail(FirstName,LastName,Contact, Salary, deptID, Address) 
+values
+('Shruti','Ingle',8483831723,23000,11,'Nagr'),
+('pallavi','Kamble',8483831723,20000,12,'Nagpur'),
+('Priyanka','Waidya',8483834773,30000,12,'Nagpur'),
+('anisha','raj',8482331723,40000,12,'wardha'),
+('anushka','take',8422831723,40000,12,'Nagpur'),
+('sarvesh','singh',8483831723,50000,12,'mumbai'),
+('Pooja','mishra',8274831723,10000,12,'Nagpur'),
+('aditi','sharma',8483831723,23000,12,'Nagr');
