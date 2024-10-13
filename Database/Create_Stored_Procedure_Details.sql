3ff4bc5ef3ab6545ff3b43f77ce9e0d76032c7e8
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-13 15:06:26 +0530

    The stored procedure for table procedure details

---
+++ Database/Create_Stored_Procedure_Details.sql
@@ -0,0 +1,15 @@
+create  procedure details
+@FirstName varchar(50),
+@LastName varchar(50),
+@City varchar(50),
+@Country varchar(50),
+@Phone varchar(50),
+@Email varchar(25),
+@countryname varchar(50),
+@regionid varchar(50)
+as
+begin
+insert into Customerdetail(FirstName,LastName,City,Country,Phone,Email) values(@FirstName,@LastName,@City,@Country,@Phone,@Email)
+insert into countries(countryname,regionid) values(@countryname,@regionid)
+end
+exec details @FirstName='Shruti',@LastName='Ingle',@City='Nagpur',@Country='India',@Phone='987654',@Email='shruti@123',@countryname='India',@regionid='259285';
--- Database/selet_Columns_from_Table.sql
+++ Database/selet_Columns_from_Table.sql
@@ -1,1 +1,1 @@
-Select  StudentName,StudentId From tb_StudentDetails where StudentId>101 and StudentId<103;
+Select  StudentName,StudentId From tb_StudentDetails where StudentId>101 and StudentId<103
