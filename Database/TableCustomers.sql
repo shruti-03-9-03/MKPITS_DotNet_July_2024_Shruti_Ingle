37885df9c990901f5c0cc7d1590af814cd42d9d5
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-15 17:26:00 +0530

    The table customers

---
+++ Database/TableCustomers.sql
@@ -0,0 +1,5 @@
+create table Customers(CustomerID Bigint,CustomerName varchar(55),Email varchar(55));
+
+insert into Customers(CustomerID,CustomerName,Email)values(1,'John','John@Example.com');
+insert into Customers(CustomerID,CustomerName,Email)values(2,'jane smith','jane@example.com');
+select*from Customers;
