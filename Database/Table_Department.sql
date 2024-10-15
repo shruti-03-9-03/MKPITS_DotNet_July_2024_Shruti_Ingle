e933770f4479c127db6c4a210f15a9a0c8aa783a
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-15 17:24:36 +0530

    The table Department

---
+++ Database/Table_Department.sql
@@ -0,0 +1,6 @@
+create table Department(DepartmentID varchar (55),DepartmentName varchar(55));
+
+insert into Department(DepartmentID,DepartmentName)values(101,'HR');
+insert into Department(DepartmentID,DepartmentName)values(102,'IT');
+insert into Department(DepartmentID,DepartmentName)values(103,'Marketing');
+select*from Department;
