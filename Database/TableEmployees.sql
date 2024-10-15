fb389d7e99c750359986fb8c00aee8151c5f06f1
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-15 17:23:04 +0530

    The TableEmaployees

---
+++ Database/TableEmployees.sql
@@ -0,0 +1,5 @@
+create table Employees(EmployeeID varchar(55),Name varchar(55),DepartmentID varchar(55));
+insert into Employees(EmployeeID,Name,DepartmentID)values(1,'John Doe',101);
+insert into Employees(EmployeeID,Name,DepartmentID)values(2,'Jane Smith',102);
+insert into Employees(EmployeeID,Name,DepartmentID)values(3,'Alex Brown',103);
+select*from Employees;
