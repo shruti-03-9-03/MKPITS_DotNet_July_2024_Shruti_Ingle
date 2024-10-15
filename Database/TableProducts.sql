a6566d0e44c6a3d8d7f8ed2a4e1d6a660453de4b
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-15 17:32:24 +0530

    The TableProducts

---
+++ Database/TableProducts.sql
@@ -0,0 +1,7 @@
+create table Products(ProductID bigint,ProductName varchar(55),Price bigint);
+
+insert into Products values(201,'Laptop',1000);
+insert into Products values(202,'Mouse',50);
+insert into Products values(203,'Keyboard',75);
+
+select*from Products;
