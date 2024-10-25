2f76811340484c238cee160aa015c3bbcc8a4bf6
Shruti Ingle <ishrutiingle@gmail.com>
2024-10-25 23:18:59 +0530

    Secondlargest and fahrenheit

---
+++ cpp/Second_Largest_Element_in_an_Array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0], secondLargest = -1;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    cout << "Second largest element: " << secondLargest << endl;
+
+    return 0;
+}
+// Second Largest Element in an Array
